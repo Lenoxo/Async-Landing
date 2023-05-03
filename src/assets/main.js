e7bf262e63e871615271b5b4a89919aeb602ae89
Emanuel Padilla <emanuehl159@gmail.com>
2023-05-02 19:37:50 -0500

    Avances hasta la parte 22 del curso, con mis ajustes para presentar como proyecto

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,20 +1,30 @@
 // Selecciono la parte del contenido
-const content = null || document.getElementById('content')
+const contentYoutube = null || document.getElementById('content-youtube')
+const contentImdb = null || document.getElementById('content-imdb')
 // Guardo la url de la API
-const url = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC-5MT-BUxTzkPTWMediyV0w&part=snippet%2Cid&order=date&maxResults=5';
+const APIyoutube = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC-5MT-BUxTzkPTWMediyV0w&part=snippet%2Cid&order=date&maxResults=8';
+const APIimdb = 'https://imdb-top-100-movies.p.rapidapi.com/';
 // Opciones por defecto de RapidAPI
-const options = {
+const optionsYoutube = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '50254dd43dmsh38fdebeb3525ba9p1e7991jsnb4d98105266d',
 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 };
+const optionsImdb = {
+  method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '50254dd43dmsh38fdebeb3525ba9p1e7991jsnb4d98105266d',
+		'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+	}
+}
+// Opciones por defecto de RapidAPI
 
 // Función asincrona para obtener los videos y convertirlos a forma .json
-async function fetchVideos() {
+async function fetchData(urlApi, options) {
     try {
-        const response = await fetch(url, options);
+        const response = await fetch(urlApi, options);
         const result = await response.json();
         return result;
     } catch (error) {
@@ -25,7 +35,7 @@ async function fetchVideos() {
 // Función asincrona que se llama en el momento para escribir de forma dinámica en el DOM los videos
 (async () => {
   try {
-    const videos = await fetchVideos()
+    const videos = await fetchData(APIyoutube, optionsYoutube)
     let view = `
       ${videos.items.map(video => `
       <div class="group relative">
@@ -40,12 +50,32 @@ async function fetchVideos() {
           </h3>
         </div>
       </div>
-      `).slice(0,4).join('')}
+      `).slice(0,8).join('')}
     `;
     /* 
     Para los videos, recorto del primer al quinto video usando slice(0,4), y join('') para juntar nuevamente el view.
     */
-    content.innerHTML = view
+    contentYoutube.innerHTML = view
+    console.log(videos)
+    const movies = await fetchData(APIimdb, optionsImdb)
+    console.log(movies)
+    view = `
+    ${movies.map(movie => `
+    <div class="group relative">
+      <div
+        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+        <img src="${movie.image}" alt="${movie.title}" class="w-full">
+      </div>
+      <div class="mt-4 flex justify-between">
+        <h3 class="text-sm text-gray-700">
+          <span aria-hidden="true" class="absolute inset-0"></span>
+          ${movie.title}
+        </h3>
+      </div>
+    </div>
+    `).slice(0,8).join('')}
+  `;
+    contentImdb.innerHTML = view
   } catch (error) {
     console.log(error)
   }
